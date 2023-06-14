59721ba481473b9acd76b1c5e458bcb76b5e99f2
Michael Hubbard <michael.hubbard@open.ac.uk>
2023-06-14 17:43:46 +0100

    Tutorial-3; Serving HTML

--- Flask-app/app/admin_views.py
+++ Flask-app/app/admin_views.py
@@ -1,9 +1,11 @@
 from app import app
 
+from flask import render_template
+
 
 @app.route("/admin/dashboard")
 def admin_dashboard():
-    return "Admin dashboard"
+    return render_template("admin/dashboard.html")
 
 
 @app.route("/admin/profile")
