e179ce6f331bec60ed5e71b143118d5bccefd15a
Michael Hubbard <michael.hubbard@open.ac.uk>
2023-06-14 17:42:06 +0100

    Tutorial-2; App structure

--- Flask-app/app/views.py
+++ Flask-app/app/views.py
@@ -1,14 +1,11 @@
-from flask import Flask
+from app import app
 
-app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello, World!"
 
+
 @app.route("/about")
 def about():
     return "<h1 style='color: red'> About !!!</h1>"
-
-if __name__ == "__main__":
-    app.run()
