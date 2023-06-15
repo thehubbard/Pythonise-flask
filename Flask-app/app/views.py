9d08e89c9732b0490739ccf090dd9e0cd1eab3b9
Michael Hubbard <michael.hubbard@open.ac.uk>
2023-06-15 13:16:40 +0100

    Tutorial-13; image uploads

--- Flask-app/app/views.py
+++ Flask-app/app/views.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+import os
 
 from flask import render_template, request, redirect, jsonify, make_response
+from werkzeug.utils import secure_filename
 from app import app
 
 
@@ -172,3 +174,53 @@ def query():
 
 
 # ?foo=foo&bar=bar&baz=baz&title=query+strings+with+flask
+
+app.config[
+    "IMAGE_UPLOADS"
+] = f"{os.path.dirname(os.path.realpath(__file__))}/static/img/uploads"
+
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG", "GIF"]
+app.config["MAX_IMG_FILESIZE"] = 0.5 * 1024 * 1024
+
+
+def allowed_image(filename):
+    if "." not in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
+
+def allowed_image_filesize(filesize):
+    return True if int(filesize) <= app.config["MAX_IMG_FILESIZE"] else False
+
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+    if (request.method == "POST") and (request.files):
+        image = request.files["image"]
+
+        if not allowed_image_filesize(request.cookies.get("filesize")):
+            print("File exceeded maximum size")
+
+        if image.filename == "":
+            print("Image must have a filename")
+
+        if not allowed_image(image.filename):
+            print("That file extension is not allowed")
+            return redirect(request.url)
+        else:
+            filename = secure_filename(image.filename)
+
+            image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
+
+        print("Image saved")
+        print(image.content_length)
+
+        return redirect(request.url)
+
+    return render_template("public/upload_image.html")
